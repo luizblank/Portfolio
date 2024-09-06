ce0492db124512d848526c7d8efff7d50a00fb27
luizblank <luizrosacardoso@gmail.com>
2024-09-06 16:54:04 -0300

    Responsive character walk

--- src/constants/Actions.jsx
+++
@@ -1,8 +0,0 @@
-import { animations } from "./Animations";
-
-export const movements = [
-    { id: 0, x: 0, y: 0, start_anm: animations.static, final_anm: animations.static, duration: 0, delay: 1, scaleX: 1 },
-    { id: 1, x: 200, y: 0, start_anm: animations.walking, final_anm: animations.talking, duration: 1.5, delay: 0, scaleX: 1, text: "vou passar o slide" },
-    { id: 2, x: 1200, y: 0, start_anm: animations.walking, final_anm: animations.static, duration: 2, delay: 0, scaleX: 1, action: "nextSlide", action_time: 2000 },
-    { id: 3, x: 100, y: 0, start_anm: animations.walking, final_anm: animations.static, duration: 2, delay: 1, scaleX: -1 },
-]
--- src/pages/Animation/index.jsx
+++ src/pages/Animation/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { movements } from '../../constants/Actions';
+import { movements } from '../../constants/Movements';
 import { sprites_sizes } from '../../constants/SpritesSizes';
 import { Sprite, Dialog, Ground } from '../../constants/Styles';
 import { backgrounds } from '../../constants/Backgrounds';
@@ -11,17 +11,29 @@ export default function Animation() {
 	const [firstMovement, setFirstMovement] = useState(
 		sessionStorage.getItem('last_movement') ? parseInt(sessionStorage.getItem('last_movement')) : 0
 	);
-
+	const [windowDimensions, setWindowDimensions] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight,
+	});
 	const [crrMovement, setCrrMovement] = useState(movements[firstMovement]);
 	const [crrSprite, setCrrSprite] = useState(crrMovement.start_anm.url);
-
 	const [showDialog, setShowDialog] = useState(crrMovement.text ? true : false);
-
 	const [currentIndex, setCurrentIndex] = useState(0);
 
 	useEffect(() => {
 		setCrrSprite(crrMovement.start_anm.url);
 	}, [crrMovement]);
+	
+	useEffect(() => {
+		const handleResize = () => {
+		setWindowDimensions({
+			width: window.innerWidth,
+			height: window.innerHeight,
+		});
+		};
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
 
 	function handleAnimationComplete() {
 		if (crrMovement.id < movements.length - 1) {
@@ -33,17 +45,17 @@ export default function Animation() {
 	function handleDialogClick() {
 		setShowDialog(false);
 		handleAnimationComplete();
-	}
+	};
 
 	function nextSlide() {
 		setCurrentIndex((prevIndex) => (prevIndex + 1) % backgrounds.length);
 		handleAnimationComplete();
-	}
+	};
 	
 	function prevSlide() {
 		setCurrentIndex((prevIndex) => prevIndex - 1 < 0 ? backgrounds.length - 1 : prevIndex - 1 );
 		handleAnimationComplete();
-	}
+	};
 
 	return (
 		<>
@@ -70,8 +82,8 @@ export default function Animation() {
 						onClick={() => handleDialogClick()}
 						style={{ display: showDialog ? 'flex' : 'none' }}
 						animate={{
-							x: crrMovement.x - ((sprites_sizes.dialog.int_width - sprites_sizes.character.int_width) / 2),
-							y: crrMovement.y
+							x: (windowDimensions.width * crrMovement.x) - (sprites_sizes.character.int_width / 2) - ((sprites_sizes.dialog.int_width - sprites_sizes.character.int_width) / 2),
+							y: windowDimensions.height * crrMovement.y
 						}}
 						transition={{ type: 'tween', duration: crrMovement.duration, delay: crrMovement.delay }}
 					>
@@ -83,22 +95,27 @@ export default function Animation() {
 						alt='sprite'
 						src={crrSprite}
 						style={{ scaleX: crrMovement.scaleX }}
-						animate={{ x: crrMovement.x, y: crrMovement.y }}
+						animate={{
+							x: (windowDimensions.width * crrMovement.x) - (sprites_sizes.character.int_width / 2),
+							y: windowDimensions.height * crrMovement.y
+						}}
 						transition={{ type: 'tween', duration: crrMovement.duration, delay: crrMovement.delay }}
 						onAnimationComplete={() => {
-							if (crrMovement.text != null) {
-								setCrrSprite(crrMovement.final_anm.url);
-								setShowDialog(true);
-								return;
-							}
 							if (crrMovement.action != null) {
 								setCrrSprite(crrMovement.final_anm.url);
 								setTimeout(() => {
-									if (crrMovement.action == "nextSlide")
-									nextSlide();
-									if (crrMovement.action == "prevSlide")
+									if (crrMovement.action == "nextSlide") {
+										nextSlide();
+									}
+									if (crrMovement.action == "prevSlide") {
 										prevSlide();
+									}
 								}, crrMovement.action_time);
+								return;
+							}
+							if (crrMovement.text != null) {
+								setCrrSprite(crrMovement.final_anm.url);
+								setShowDialog(true);
 								return;
 							}
 							handleAnimationComplete();
