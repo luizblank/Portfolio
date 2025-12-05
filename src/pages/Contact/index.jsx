2966573cee902e677a68490cb506c5588af700a8
Luiz <102316355+luizblank@users.noreply.github.com>
2025-12-05 15:07:58 -0300

    Update email link in Contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -11,7 +11,7 @@ export default function Contact() {
                     <div className={styles.back}>
                         <a className={styles.redirect} href="https://github.com/luizblank" target="_blank"><FaGithub color="white"/></a>
                         <a className={styles.redirect} href="https://www.linkedin.com/in/luiz-antonio-rosa-cardoso-9005512b7/" target="_blank"><FaLinkedin className={styles.icon}/></a>
-                        <a href=""><MdEmail className={styles.icon}/></a>
+                        <a href="mailto:luizrosacardoso@gmail.com"><MdEmail className={styles.icon}/></a>
                     </div>
                     <div className={styles.front}>me</div>
                 </h1>
